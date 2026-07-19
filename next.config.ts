35aab9b0202b98b935b040500e66b80624265c40
lokesh4877saini <genu08saini@gmail.com>
2026-07-20 01:17:57 +0530

    updates configs

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        pathname: '/seed/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/mrlokesh/**',
       }
     ]
   }
